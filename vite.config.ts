94b338c54b6115f61184b1f830d79d3b6faa5852
marcoslafoz <marcos.lafoz96@gmail.com>
2024-05-18 14:02:12 +0200

    build: :globe_with_meridians: pwa library installed (custom manifest)

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,39 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { VitePWA } from 'vite-plugin-pwa'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-    }
-  },
   server: {
     port: 3000
-  }
+  },
+  plugins: [react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'Student Space',
+        short_name: 'StudentSpace',
+        description: 'StudentSpace for desktop!',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'favicon.svg',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'favicon.svg',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+
 })

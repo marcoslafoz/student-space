3fcc9ec7f4df562e0edf0867caf1aaa60682281d
marcoslafoz <marcos.lafoz96@gmail.com>
2024-12-04 19:46:51 +0000

    ubuntu-server fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
-const { nextui } = require("@nextui-org/react");
+// Usa la sintaxis de importación de ESM
+import { nextui } from "@nextui-org/react";
+import flowbitePlugin from 'flowbite/plugin';
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [ 
+export default {
+  content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
@@ -15,6 +17,7 @@ module.exports = {
   darkMode: "class",
   plugins: [
     nextui(),
-    require('flowbite/plugin'), 
-  ]
-}
+    flowbitePlugin,
+  ],
+};
+

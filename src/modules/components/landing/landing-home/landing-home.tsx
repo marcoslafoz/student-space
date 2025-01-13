da81a06b7c7008b60b6e310eaa3d6a6ddd6d0ea2
marcoslafoz <marcos.lafoz96@gmail.com>
2025-01-13 19:01:29 +0100

    style: some linting fix

--- src/modules/components/landing/landing-home/landing-home.tsx
+++ src/modules/components/landing/landing-home/landing-home.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './landing-home.scss'
 import StudentGirl from '/assets/images/webp/girl-student.webp'
-import { ScrollDownArrow } from '../../base/scroll-down-arrow'
 
 export const LandingHome: React.FC = () => {
   return (

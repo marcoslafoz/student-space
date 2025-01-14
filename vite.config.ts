b0804156d05fe88a1f16898e87f1928f6389081c
marcoslafoz <marcos.lafoz96@gmail.com>
2025-01-14 01:22:47 +0100

    feat: manifest + favicon updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,31 +7,6 @@ export default defineConfig({
     port: 3000
   },
   plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true,
-      },
-      manifest: {
-        name: 'Student Space',
-        short_name: 'StudentSpace',
-        description: 'StudentSpace for desktop!',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'favicon.svg',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'favicon.svg',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-    }),
+    react()
   ],
-
 })

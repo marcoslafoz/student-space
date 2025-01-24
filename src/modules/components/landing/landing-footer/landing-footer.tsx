4857215d925343d435fa79c7a285490adb00841f
marcoslafoz <marcos.lafoz96@gmail.com>
2025-01-24 12:56:06 +0100

    feat: footer updated

--- src/modules/components/landing/landing-footer/landing-footer.tsx
+++ src/modules/components/landing/landing-footer/landing-footer.tsx
@@ -4,9 +4,7 @@ import './landing-footer.scss'
 export const LandingFooter: React.FC = () => {
   return (
     <>
-      <div className='landing-footer w-full'>
-        <span className='text-gray-500'>Footer</span>
-      </div>
+      <div className='landing-footer custom-bg-color-primary w-full'></div>
     </>
   )
 }
--- src/modules/components/landing/landing-view/landing-view.tsx
+++ src/modules/components/landing/landing-view/landing-view.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
 import { LandingHome } from '../landing-home'
-import { LandingDemo } from '../landing-demo'
+// import { LandingDemo } from '../landing-demo'
 import { TextLogo } from '../../base'
 import { Link } from 'react-router-dom'
 import { Helmet } from 'react-helmet'
+import { LandingFooter } from '../landing-footer'
 
 export const LandingView: React.FC = () => {
   return (
@@ -27,8 +28,8 @@ export const LandingView: React.FC = () => {
         </div>
 
         <LandingHome />
-        <LandingDemo />
-        {/* <LandingFooter /> */}
+        {/* <LandingDemo /> */}
+        <LandingFooter />
       </div>
     </>
   )

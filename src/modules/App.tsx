0961c91052937737e0ba777235f0e91fd492ca3e
marcoslafoz <marcos.lafoz96@gmail.com>
2025-01-28 01:03:57 +0100

    feat: login view style improve

--- src/modules/App.tsx
+++ src/modules/App.tsx
@@ -3,7 +3,7 @@ import { router } from '../common/router'
 
 export const App: React.FC = () => {
   return (
-    <main className='custom-color-primary dark:text-slate-300'>
+    <main className='custom-color-primary'>
       <RouterProvider router={router} />
     </main>
   )

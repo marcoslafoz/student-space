8fde911f409472af6c64fc6ec0c134f439f94e55
marcoslafoz <marcos.lafoz96@gmail.com>
2025-02-05 20:42:11 +0100

    style: landing-footer column on wrap

--- src/modules/components/landing/landing-footer/landing-footer.tsx
+++ src/modules/components/landing/landing-footer/landing-footer.tsx
@@ -4,10 +4,10 @@ import { InstagramIcon, TiktokIcon, XIcon } from '../../base'
 export const LandingFooter: React.FC = () => {
   return (
     <>
-      <div className='flex flex-col items-center  custom-bg-color-primary w-full  '>
-        <div className=' flex flex-col w-2/3 gap-5 h-auto pb-10'>
+      <div className='flex flex-col items-center custom-bg-color-primary w-full'>
+        <div className='flex flex-col w-3/4 md:w-2/3 lg:w-2/3 gap-5 h-auto pb-10'>
           <hr className='border-[#545454] px-16 w-full' />
-          <div className='flex flex-row flex-wrap w-full items-center justify-between text-[#545454] text-sm text-balance'>
+          <div className='flex flex-col md:flex-row flex-wrap w-full items-center justify-center md:justify-between text-[#545454] text-sm text-balance gap-4'>
             <a href='/'>
               <div className='hover:text-[#747474]'>StudentSpace</div>
             </a>
@@ -17,7 +17,7 @@ export const LandingFooter: React.FC = () => {
             </div>
             <div className='hover:text-[#747474]'>Terminos del sitio web</div>
             <div className='hover:text-[#747474]'>Privacidad</div>
-            <div className='hover:text-[#747474] flex flex-row flex-wrap justify-between items-center gap-4'>
+            <div className='hover:text-[#747474] flex flex-row justify-center items-center gap-4'>
               <a href='https://www.x.com'>
                 <XIcon className='fill-[#545454] hover:fill-[#747474]' />
               </a>

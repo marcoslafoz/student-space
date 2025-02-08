322a508e2e588955adabfb4f3b68166f202a406e
marcoslafoz <marcos.lafoz96@gmail.com>
2025-02-08 16:55:24 +0100

    buid: output names rename by package version

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { version } from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react()
-  ],
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].${version}.js`,
+        chunkFileNames: `[name].${version}.js`,
+        assetFileNames: `[name].${version}.[ext]`
+      }
+    }
+  }
 })

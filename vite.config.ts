d58ff64abce9e48dd6af91a14eb52cfc9d7c1012
marcoslafoz <marcos.lafoz96@gmail.com>
2025-02-11 01:25:00 +0100

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { version } from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: `[name].${version}.js`,
-        chunkFileNames: `[name].${version}.js`,
-        assetFileNames: `[name].${version}.[ext]`
-      }
-    }
-  }
 })
